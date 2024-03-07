847ee29034b1b1e42fc4d34c2f0e2483902eadc2
Riyan est <riyanq95@gmail.com>
2024-03-07 17:43:47 +0530

    prototype

--- app.py
+++ app.py
@@ -3,17 +3,26 @@ from flask import Flask, request, jsonify, render_template
 # , redirect, url_for, flash, send_from_directory
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 import json
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from apiclient.discovery import build
-# from google_auth_oauthlib.flow import InstalledAppFlow
 import nltk
 from nltk.corpus import wordnet
 from rembg import remove
 from PIL import Image
 import os
 from werkzeug.utils import secure_filename
+
+import os
+import flask
+import requests
+from datetime import datetime
+
+# import google.oauth2.credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 import json
+
 # import os
 # import re
 # import nltk
@@ -29,23 +38,41 @@ import json
 # from youtube_comment_scraper_python import *
 
 
-# Arguments that need to passed to the build function
-DEVELOPER_KEY = "AIzaSyCKZ9Y5geIBmjNFgZMhl1-Yh3IX0C1yEpw"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
+# Arguments that need to passed to the buil
 
-# creating Youtube Resource Object
-youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-                developerKey=DEVELOPER_KEY)
+API_SERVICE_NAME = "youtubeAnalytics"
+API_VERSION = "v2"
+CLIENT_SECRETS_FILE = 'D:/Projects/ytToolkit/Sentiment_analysis/client_secret_361851587388-5dmhhj1ctokd64u7stbff0g7ctg0j932.apps.googleusercontent.com.json'
+# Load your downloaded JSON credentials file
+
+# 361851587388-5dmhhj1ctokd64u7stbff0g7ctg0j932.apps.googleusercontent.com
+
+SCOPES = ['https://www.googleapis.com/auth/yt-analytics.readonly']
+
+
+def get_service():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_local_server()
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+
+def execute_api_request(client_library_function, **kwargs):
+    response = client_library_function(**kwargs).execute()
+    print(response)
+    return response
+
+
+today = datetime.today().strftime('%Y-%m-%d')
 
 
 app = Flask(__name__)
 sentiments = SentimentIntensityAnalyzer()
 
+api_key = "AIzaSyCKZ9Y5geIBmjNFgZMhl1-Yh3IX0C1yEpw"
 
 # import pandas as pd
-
-api_key = "AIzaSyCKZ9Y5geIBmjNFgZMhl1-Yh3IX0C1yEpw"
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 def video_comments(video_id):
@@ -55,7 +82,6 @@ def video_comments(video_id):
     replies = []
 
     # creating youtube resource object
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     # retrieve youtube video results
     video_response = youtube.commentThreads().list(
@@ -112,6 +138,14 @@ def video_comments(video_id):
             break
 
 
+video_details = {
+    'videoStats': "none",
+    'channelStats': "none",
+    'bg': "none",
+    'sentiments': {},
+    'tags': []
+}
+
 # Enter video id
 video_id = "II1ig6XotTU"
 
@@ -148,13 +182,18 @@ def analyze_sentiments(text):
     finalcommentdf = data.merge(
         sentimentdf, left_index=True, right_index=True, how='inner')
     # print(finalcommentdf['comment'].tolist())
+    Finalsentiments = {}
+    # print(finalcommentdf['comment'][0])
     comments = finalcommentdf['comment'].tolist()
     sentimentslist = finalcommentdf['sentiment'].tolist()
-#     print(finalcommentdf.head())
-    json_string = json.dumps(
-        {'comments': comments, 'sentiment': sentimentslist})
+    for i in range(0, len(finalcommentdf)):
+        # Finalsentiments.append((finalcommentdf['comment'][i]: finalcommentdf['sentiment'][i]))
+        Finalsentiments[comments[i]] = sentimentslist[i]
 
-    return json_string
+    print(finalcommentdf)
+    # json_string = {'comments': comments, 'sentiment': sentimentslist}
+
+    return Finalsentiments
 
 #     print(datasentiments)
 
@@ -174,13 +213,121 @@ def analyze_sentiment(text):
 
 @app.route("/")
 def index():
-    return "Hello"
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv")
+    df.columns = [col.replace("AAPL.", "") for col in df.columns]
+
+    # Create figure
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(x=list(df.Date), y=list(df.High)))
+
+    # Set title
+    fig.update_layout(
+        title_text="Time series with range slider and selectors"
+    )
+
+    # Add range slider
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+    )
+
+    graphJSON = fig.to_json()
+
+    return render_template('index.html', graphJSON=graphJSON)
 
 
 @app.route("/dashboard")
 def dashboard():
     message = "Hello, World"
-    return render_template('dashboard.html', message=message)
+    return render_template('dashboard.html', response=video_details)
+
+
+@app.route('/channelStats', methods=['POST'])
+def channelstats():
+    data = request.form.get('id')  # Expect JSON data with a 'text' field
+
+    if data is not None:
+        ytrequest = youtube.channels().list(
+            part="snippet,contentDetails,statistics", id=data)
+
+        video_details['channelStats'] = ytrequest.execute()[
+            'items'][0]['snippet']
+        print(video_details['channelStats'])
+        # return render_template('channelData.html', response=video_details)
+    else:
+        return jsonify({'error': 'Missing or invalid data'})
+
+
+@app.route('/videoStats', methods=['POST', 'GET'])
+def videostats():  # Expect JSON data with a 'text' field+
+    id = request.form.get('id')
+
+    if id is not None:
+        ytrequest = youtube.videos().list(
+            part="snippet,contentDetails,statistics", id=id)
+        ytresponse = ytrequest.execute()
+        print(ytresponse)
+        snippet_details = ytresponse['items'][0]['snippet']
+
+        thumbnail_urls = {}
+        thumbnails = snippet_details['thumbnails']
+        for size, data in thumbnails.items():
+            thumbnail_urls[size] = data['url']
+# Storing in a dictionary
+        video_details['videoStats'] = {
+            'title': snippet_details['title'],
+            'published_at': snippet_details['publishedAt'],
+            'channel_id': snippet_details['channelId'],
+            'description': snippet_details['description'],
+            'thumbnails': thumbnail_urls,
+            'channel_title': snippet_details['channelTitle'],
+            'tags': snippet_details['tags'],
+            'category_id': snippet_details['categoryId'],
+            'live_broadcast_content': snippet_details['liveBroadcastContent'],
+            'localized_title': snippet_details['localized']['title'],
+            'localized_description': snippet_details['localized']['description'],
+            'viewCount': ytresponse['items'][0]['statistics']['viewCount'],
+            'likeCount': ytresponse['items'][0]['statistics']['likeCount'],
+            'favoriteCount': ytresponse['items'][0]['statistics']['favoriteCount'],
+            'commentsCount': ytresponse['items'][0]['statistics']['commentCount'],
+            # 'default_audio_language': snippet_details['defaultAudioLanguage']
+        }
+
+        data = video_comments(id)
+        video_details['sentiments'] = analyze_sentiments(data)
+
+        # print(ytresponse['items'][0]['statistics']['viewCount']+" testtt")
+        return render_template('dashboard.html', response=video_details)
+    else:
+        return render_template('dashboard.html', response="none")
 
 
 @app.route("/map")
@@ -226,75 +373,6 @@ def analyze_sentiment_route():
         return jsonify({'error': 'Missing or invalid data'})
 
 
-@app.route('/analyze_sentiments', methods=['POST'])
-def analyze_sentiments_route():
-    data = request.json  # Expect JSON data with a 'text' field
-    text = data.get('text')
-
-    if text is not None:
-        data = video_comments(text)
-        final = analyze_sentiments(data)
-        return jsonify({'data': final})
-    else:
-        return jsonify({'error': 'Missing or invalid data'})
-
-
-@app.route('/channelStats', methods=['POST'])
-def channelstats():
-    data = request.json  # Expect JSON data with a 'text' field
-    text = data.get('text')
-
-    if text is not None:
-        ytrequest = youtube.channels().list(
-            part="snippet,contentDetails,statistics", id=text)
-
-        ytresponse = ytrequest.execute()
-        return jsonify({'channelData': ytresponse})
-    else:
-        return jsonify({'error': 'Missing or invalid data'})
-
-
-@app.route('/videoStats', methods=['POST', 'GET'])
-def videostats():  # Expect JSON data with a 'text' field
-    id = request.form.get('id')
-
-    if id is not None:
-        ytrequest = youtube.videos().list(
-            part="snippet,contentDetails,statistics", id=id)
-        ytresponse = ytrequest.execute()
-        print(ytresponse)
-        snippet_details = ytresponse['items'][0]['snippet']
-
-        thumbnail_urls = {}
-        thumbnails = snippet_details['thumbnails']
-        for size, data in thumbnails.items():
-            thumbnail_urls[size] = data['url']
-# Storing in a dictionary
-        video_details = {
-            'title': snippet_details['title'],
-            'published_at': snippet_details['publishedAt'],
-            'channel_id': snippet_details['channelId'],
-            'description': snippet_details['description'],
-            'thumbnails': thumbnail_urls,
-            'channel_title': snippet_details['channelTitle'],
-            'tags': snippet_details['tags'],
-            'category_id': snippet_details['categoryId'],
-            'live_broadcast_content': snippet_details['liveBroadcastContent'],
-            'localized_title': snippet_details['localized']['title'],
-            'localized_description': snippet_details['localized']['description'],
-            'viewCount': ytresponse['items'][0]['statistics']['viewCount'],
-            'likeCount': ytresponse['items'][0]['statistics']['likeCount'],
-            'favoriteCount': ytresponse['items'][0]['statistics']['favoriteCount'],
-            'commentsCount': ytresponse['items'][0]['statistics']['commentCount'],
-            # 'default_audio_language': snippet_details['defaultAudioLanguage']
-        }
-
-        # print(ytresponse['items'][0]['statistics']['viewCount']+" testtt")
-        return render_template('icons.html', response=video_details)
-    else:
-        return jsonify({'error': 'Missing or invalid data'})
-
-
 @app.route('/search', methods=['POST'])
 def search():
     data = request.json  # Expect JSON data with a 'text' field
@@ -311,13 +389,6 @@ def search():
 @app.route('/bgrm', methods=['GET', 'POST'])
 def bgrm():
     if request.method == 'POST':
-        return render_template('brgm.html')
-    return render_template('brgm.html')
-
-
-@app.route('/bgrmsuccess', methods=['POST'])
-def success():
-    if request.method == 'POST':
         f = request.files['file']
         input = Image.open(f)
         # Removing the background from the given Image
@@ -326,7 +397,8 @@ def success():
         # Saving the image in the given path
         output.save(os.path.join(
             'D:/Projects/ytToolkit/Sentiment_analysis/static/assets/img', filename), format='png')
-        return render_template('acknowledgement.html', name=filename)
+        video_details['bg'] = filename
+        return render_template('dashboard.html', response=video_details)
         # return send_from_directory('D:/Projects/ytToolkit/Sentiment_analysis/static/assets/img', filename)
         # output.save(output_path)
         # # f.save(f.filename)
@@ -354,8 +426,106 @@ def home():
                     related_terms[noun].add(lemma.name())
                     for related_synset in lemma.derivationally_related_forms():
                         print(related_terms[noun].add(related_synset.name()))
-        return render_template('tags.html', tags=related_terms)
-    return render_template('tagGen.html')
+        video_details['tags'] = related_terms
+        return render_template('dashboard.html', response=video_details)
+    return render_template('dashboard.html')
+
+
+@app.route('/analytics', methods=['GET'])
+def analytics():  # Expect JSON data with a 'text' field
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+    youtubeAnalytics = get_service()
+    response = execute_api_request(
+        youtubeAnalytics.reports().query,
+        ids='channel==MINE',
+        startDate='2018-01-01',
+        endDate='2024-03-01',
+        metrics='estimatedMinutesWatched,views,likes,subscribersGained',
+        dimensions='month')
+
+    dates = []
+    minutesWatched = []
+    views = []
+    likes = []
+    subs = []
+    for response in response['rows']:
+        dates.append(response[0])
+        minutesWatched.append(response[1])
+        views.append(response[2])
+        likes.append(response[3])
+        subs.append(response[4])
+
+# Create traces
+    traces = []
+
+    traces.append(go.Scatter(x=dates, y=minutesWatched,
+                  mode='lines+markers', name="MINUTES WATCHED"))
+    traces.append(go.Scatter(x=dates, y=views,
+                  mode='lines+markers', name="VIEWS"))
+    traces.append(go.Scatter(x=dates, y=likes,
+                  mode='lines+markers', name="LIKES"))
+    traces.append(go.Scatter(x=dates, y=subs,
+                  mode='lines+markers', name="DATES"))
+
+# Create figure
+    fig = go.Figure(data=traces)
+
+# Update layout
+    fig.update_layout(
+        title='Metrics Over Time',
+        xaxis=dict(
+            title='Month',
+            type='date',
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            )
+        ),
+        yaxis=dict(
+            title='Count'
+        )
+    )
+
+    # fig.show()
+    graphJSON = fig.to_json()
+    video_details['graphJSON'] = graphJSON
+    return render_template('dashboard.html', response=video_details)
+
+    # return jsonify({'response': response})
+    # for response in response['columnHeaders']:
+#     #     print(response['name'])
+
+
+#     # Create figure
+
+# # Show plot
+#     fig.show()
+#     return jsonify({'month': dates,
+#                     "minutesWatched": minutesWatched,
+#                     "views": views,
+#                     "likes": likes,
+#                     "subs": subs})
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,45 +1,117 @@
-# -*- coding: utf-8 -*-
-
+# Impor
+import plotly.graph_objects as go
 import os
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
+import flask
+import requests
+from datetime import datetime
 
-# google-api-core==2.7.2
-# google-api-python-client==2.46.0
-# google-auth==2.6.6
-# google-auth-httplib2==0.1.0
-# google-auth-oauthlib==0.5.1
+# import google.oauth2.credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import json
+# Define API service name and version
+API_SERVICE_NAME = "youtubeAnalytics"
+API_VERSION = "v2"
+CLIENT_SECRETS_FILE = 'D:/Projects/ytToolkit/Sentiment_analysis/client_secret_361851587388-5dmhhj1ctokd64u7stbff0g7ctg0j932.apps.googleusercontent.com.json'
+# Load your downloaded JSON credentials file
+
+# 361851587388-5dmhhj1ctokd64u7stbff0g7ctg0j932.apps.googleusercontent.com
 
 SCOPES = ['https://www.googleapis.com/auth/yt-analytics.readonly']
 
-API_SERVICE_NAME = 'youtubeAnalytics'
-API_VERSION = 'v2'
-CLIENT_SECRETS_FILE = 'secrets.json'
+
 def get_service():
-  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-  credentials = flow.run_console()
-  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+    flow = InstalledAppFlow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_local_server()
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
 
 def execute_api_request(client_library_function, **kwargs):
-  response = client_library_function(
-    **kwargs
-  ).execute()
+    response = client_library_function(**kwargs).execute()
+    print(response)
+    return response
 
-  print(response)
+
+today = datetime.today().strftime('%Y-%m-%d')
+
 
 if __name__ == '__main__':
-  # Disable OAuthlib's HTTPs verification when running locally.
-  # *DO NOT* leave this option enabled when running in production.
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    # Disable OAuthlib's HTTPs verification when running locally.
+    # *DO NOT* leave this option enabled when running in production.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-  youtubeAnalytics = get_service()
-  execute_api_request(
-    youtubeAnalytics.reports().query,
-    ids='channel==MINE',
-    startDate='2017-01-01',
-    endDate='2017-12-31',
-    metrics='estimatedMinutesWatched,views,likes,subscribersGained',
-    dimensions='day',sort='day')
+    youtubeAnalytics = get_service()
+    response = execute_api_request(
+        youtubeAnalytics.reports().query,
+        ids='channel==MINE',
+        startDate='2018-01-01',
+        endDate='2024-03-01',
+        metrics='estimatedMinutesWatched,views,likes,subscribersGained',
+        dimensions='month')
+
+    dates = []
+    minutesWatched = []
+    views = []
+    likes = []
+    subs = []
+    for response in response['rows']:
+        dates.append(response[0])
+        minutesWatched.append(response[1])
+        views.append(response[2])
+        likes.append(response[3])
+        subs.append(response[4])
+
+# Create traces
+    traces = []
+
+    traces.append(go.Scatter(x=dates, y=minutesWatched,
+                  mode='lines+markers', name="MINUTES WATCHED"))
+    traces.append(go.Scatter(x=dates, y=views,
+                  mode='lines+markers', name="VIEWS"))
+    traces.append(go.Scatter(x=dates, y=likes,
+                  mode='lines+markers', name="LIKES"))
+    traces.append(go.Scatter(x=dates, y=subs,
+                  mode='lines+markers', name="DATES"))
+
+# Create figure
+    fig = go.Figure(data=traces)
+
+# Update layout
+    fig.update_layout(
+        title='Metrics Over Time',
+        xaxis=dict(
+            title='Month',
+            type='date',
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            )
+        ),
+        yaxis=dict(
+            title='Count'
+        )
+    )
+
+# Show plot
+    fig.show()
